8b46bae65f04a3373eca14e4477898efee88174b
Deepak Kumar Singh <141207306+Deepakkumar095@users.noreply.github.com>
2026-05-03 15:45:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
 import authRouter from './routes/auth.routes.js';
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
@@ -7,32 +7,37 @@ import recordRouter from './routes/record.routes.js';
 import userRouter from './routes/user.routes.js';
 import dashboardRouter from './routes/dashboard.routes.js';
 import cors from "cors";
+
 dotenv.config();
 
-
-let app = express();
+const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL
+  origin: [
+    "http://localhost:5173",
+    "https://magnificent-sfogliatella-a8dc5d.netlify.app/"
+  ],
   credentials: true
 }));
 
-let port = process.env.PORT || 8000;
-
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+
 app.use("/api", authRouter);
-
 app.use("/api/records", recordRouter);
-
 app.use("/api/dashboard", dashboardRouter);
-
 app.use("/api/users", userRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
-app.listen(port,() =>{
-    connectDB();
-    console.log(`Server is running on port ${port}`);
-})
+
+const port = process.env.PORT || 8000;
+
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}).catch((err) => {
+  console.log("DB error:", err);
+});
